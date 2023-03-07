08928cf48cc98b4700e71e9ee0047cace05c31e9
Dišlovaja <sofija.dyshlovaya@gmail.com>
2023-03-07 09:01:53 +0200

    Realizēta iespēja 1x mēst kauliņu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,9 +1,10 @@
+import java.util.Random;
 
 public class Cirks {
-
 	public static void main(String[] args) {
-		
-
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
 	}
-
 }
