32b1ce03df5f3598e4c9b8974d7b069a5e6a0003
Dišlovaja <sofija.dyshlovaya@gmail.com>
2023-03-20 16:03:13 +0200

    Lokālajā sZarā papildināta programma

--- src/Cirks.java
+++ src/Cirks.java
@@ -2,24 +2,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Cirks {
-	static void mestKaulinu(int reizes){
-		int skaitlis;
+	static void mestKaulinu(int reizes) {
+		int skaitlis, lielakais = 0;
 		Random rand = new Random();
-		for(int i=0; i<reizes; i++){
-			skaitlis = rand.nextInt(6)+1;
-			System.out.println("Uzkrita skaitlis: "+skaitlis);			
+		for (int i = 0; i < reizes; i++) {
+			skaitlis = rand.nextInt(6) + 1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis);
+
+			if (lielakais < skaitlis) {
+				lielakais = skaitlis;
+			}
 		}
-}
-	
-	
-	
+		System.out.println("Lielâkais skaitlis metienos ir " + lielakais);
+	}
+
 	public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
-		do{
+		do {
 			System.out.println("Cik reizes mest kauliòu?");
 			reizes = scan.nextInt();
-		}while(reizes<1);
+		} while (reizes < 1);
 		mestKaulinu(reizes);
 		scan.close();
 	}
